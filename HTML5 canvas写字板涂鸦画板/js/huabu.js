63a23e535bea87133ea5887df33c2bfe2a247cb3
Sola <zhouqillf@163.com>
2020-05-10 20:45:54 +0800

    :sparkles: (补充记录绘画每一笔坐标数据记录)

--- HTML5 canvas写字板涂鸦画板/js/huabu.js
+++ HTML5 canvas写字板涂鸦画板/js/huabu.js
@@ -3,6 +3,8 @@ var context;//定义全局context
 var img1=[];//储存图像数组，用于撤销
 var canX;//画布左上角的x坐标
 var canY;//画布左上角y坐标
+var drawLines = []; // 具体每一个笔画
+
 $(function(){
 	canvas = $('#cavs')[0];//获取画布的dom
 	context = canvas.getContext('2d');//获取context
@@ -37,6 +39,7 @@ $(function(){
 		xiazai.draw();
 	});
 });
+
 var Line={
 	name:"line",
 	draw:function(){
@@ -51,8 +54,21 @@ var Line={
 			if(painting===true){//判断是否是可绘画状态
 				var x = e.pageX;//鼠标当前x坐标
 				var y = e.pageY;//鼠标当前y坐标
-				context.lineTo(x-canX,y-canY);//确定线的结束位置，canvas.offsetLeft画板离浏览器左侧的距离，canvas.offsetTop画板离浏览器上部的距离
+				context.lineTo(x-canX, y-canY);//确定线的结束位置，canvas.offsetLeft画板离浏览器左侧的距离，canvas.offsetTop画板离浏览器上部的距离
+				console.log('mousemove-x', x - canX );
+				console.log('mousemove-x', y - canY);
+				let point = {
+					x: x - canX,
+					y: y - canY,
+				}
+				if (drawLines.length > 0) {
+					let lastLine = drawLines[drawLines.length - 1];
+					lastLine.push(point);
+					console.log('mousemove-drawLines', drawLines);
+				}
+
 				context.stroke();
+
 			}
 		});
 		$('#cavs').mousedown(function(e){//当鼠标按下时触发
@@ -61,11 +77,22 @@ var Line={
 			p_y = e.pageY;//画笔起始y坐标
 			context.beginPath();//开始作画
 			context.moveTo(p_x-canX,p_y-canY);//画笔开始位置
+
+			console.log('x', p_x - canX);
+			console.log('y', p_y - canY);
+
+			let startPoint = {
+				x: p_x - canX,
+				y: p_y - canY,
+			}
+			drawLines.push([startPoint]);
+			console.log('mousedown-drawLines', drawLines);
 			$('#cavs').css('cursor','pointer');//将鼠标图形设置成小手
 			//复制图像，为了撤销
 			var imgData=context.getImageData(0,0,canvas.width,canvas.height);
 			//加入数组
 			img1.push(imgData);
+			console.log('img1', img1 );
 		});
 		$('#cavs').mouseup(function(e){
 			painting = false;//鼠标松开，禁止作画
